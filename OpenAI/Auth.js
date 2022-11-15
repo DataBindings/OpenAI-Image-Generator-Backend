b8e99306a77b82f91e0a12ed61a6132304334a1b
Jeremie Pedden <59879119+jeremiepedden@users.noreply.github.com>
2022-11-15 12:17:34 -0600

    Feature: Added ability to build docker containers

--- OpenAI/Auth.js
+++ OpenAI/Auth.js
@@ -1,5 +1,5 @@
-import { Configuration, OpenAIApi } from 'openai';
-import * as dotenv from 'dotenv';
+const { Configuration, OpenAIApi } = require('openai');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
@@ -10,4 +10,4 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-export default openai;
+exports.openai = openai;
